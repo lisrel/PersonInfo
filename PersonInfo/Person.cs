6b837def1bc3cea99a8b2776f509dd527ade01d8
tino lisrel billiat <tbilliat26@gmail.com>
2024-12-22 20:35:29 +0200

    Add project files.

---
+++ PersonInfo/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    internal class Person
+    {
+        private string name;    
+        private int age;
+        private string address;
+        private string position;
+        private string department;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get {
+                return age;
+            }
+            set {
+                if (value >= 0){  
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age can not be negative");
+                }
+            }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public string Department
+        {
+            get { return department; }
+            set { department = value; }
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"Address: {Address}");
+            Console.WriteLine($"Position: {Position}");
+            Console.WriteLine($"Department: {Department}");
+        }
+    }
+}
---
+++ PersonInfo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            Console.Write("Enter Person Name: ");
+            person.Name = Console.ReadLine();
+
+            Console.Write("Enter Person's Age: ");
+            int age = int.Parse(Console.ReadLine());
+
+            if (age > 0)
+            {
+                person.Age =age;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Age");
+            }
+
+
+            Console.Write("Enter Address: ");
+            person.Address = Console.ReadLine();
+
+
+            Console.Write("Enter Position: ");
+            person.Position = Console.ReadLine();
+
+            Console.Write("Enter Department: ");
+            person.Department = Console.ReadLine();
+
+
+            Console.WriteLine("\nPerson Info:");
+            person.DisplayPersonInfo();
+
+        }
+    }
+}
